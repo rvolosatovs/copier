cbc5812662b271c2dd2bd0c0e8aa4f1aeff21f21
Roman Volosatovs <rvolosatovs@riseup.net>
2018-08-21 12:34:07 +0200

    dep,nix: Setup

---
+++ default.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs>{};
+
+buildGoPackage rec {
+  name = "copier";
+
+  goPackagePath = "github.com/rvolosatovs/copier";
+
+  src = ./.;
+
+  goDeps = ./deps.nix;
+  
+  meta = with stdenv.lib; {
+    description = "Copy a file on each change";
+    license = licenses.mit;
+    homepage = https://github.com/rvolosatovs/copier;
+  };
+}
---
+++ deps.nix
@@ -0,0 +1,30 @@
+# file generated from Gopkg.lock using dep2nix (https://github.com/nixcloud/dep2nix)
+[
+  {
+    goPackagePath  = "github.com/fsnotify/fsnotify";
+    fetch = {
+      type = "git";
+      url = "https://github.com/fsnotify/fsnotify";
+      rev =  "c2828203cd70a50dcccfb2761f8b1f8ceef9a8e9";
+      sha256 = "07va9crci0ijlivbb7q57d2rz9h27zgn2fsm60spjsqpdbvyrx4g";
+    };
+  }
+  {
+    goPackagePath  = "github.com/pkg/errors";
+    fetch = {
+      type = "git";
+      url = "https://github.com/pkg/errors";
+      rev =  "816c9085562cd7ee03e7f8188a1cfd942858cded";
+      sha256 = "1ws5crb7c70wdicavl6qr4g03nn6m92zd6wwp9n2ygz5c8rmxh8k";
+    };
+  }
+  {
+    goPackagePath  = "golang.org/x/sys";
+    fetch = {
+      type = "git";
+      url = "https://go.googlesource.com/sys";
+      rev =  "1c9583448a9c3aa0f9a6a5241bf73c0bd8aafded";
+      sha256 = "0g0nc549pmdmvja4mdqh0kgvznnw6wliqmx5wrnj02l3a23vizmi";
+    };
+  }
+]
